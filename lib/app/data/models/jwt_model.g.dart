603466b69b87dbb61acf5ab503341f7cedf21f74
Danil Malandin <malandin.danil@mail.ru>
2023-11-11 13:12:24 +0500

    late init

---
+++ lib/app/data/models/jwt_model.g.dart
@@ -0,0 +1,19 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'jwt_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+_$JwtModelImpl _$$JwtModelImplFromJson(Map<String, dynamic> json) =>
+    _$JwtModelImpl(
+      json['accessToken'] as String?,
+      json['refreshToken'] as String,
+    );
+
+Map<String, dynamic> _$$JwtModelImplToJson(_$JwtModelImpl instance) =>
+    <String, dynamic>{
+      'accessToken': instance.accessToken,
+      'refreshToken': instance.refreshToken,
+    };
---
+++ lib/app/data/services/auth_service.dart
@@ -0,0 +1,79 @@
+import 'package:dio/dio.dart';
+import 'package:get/get.dart';
+import 'package:kt8/app/core/constants.dart';
+import 'package:kt8/app/data/models/jwt_model.dart';
+import 'package:kt8/app/data/services/storage_service.dart';
+import 'package:kt8/app/routes/app_pages.dart';
+
+class AuthService extends GetxService {
+  StorageService storageService = Get.find();
+  late JWTModel _tokens;
+  Dio client = Dio(BaseOptions(baseUrl: Constants.baseUrl));
+  bool isLoggedIn = false;
+
+  String? get accessToken => _tokens.accessToken;
+  Future<bool> refresh() async {
+    print('try refresh');
+    try {
+      var res = await client.post(ApiEndpoints.refresh, data: _tokens.toJson());
+      print(res);
+      var tokens = JWTModel.fromJson(res.data);
+      await updateTokens(tokens);
+      if (res.statusCode == 200) return true;
+    } catch (e) {
+      print(e);
+    }
+    isLoggedIn = false;
+    return false;
+  }
+
+  void logout() {
+    isLoggedIn = false;
+    storageService.clear();
+    _tokens = JWTModel(null, '');
+    Get.offNamed(Routes.SIGNIN);
+  }
+
+  Future<bool> login(String email, String password) async =>
+      _auth(email, password, ApiEndpoints.login);
+
+  Future<bool> registration(String email, String password) async =>
+      _auth(email, password, ApiEndpoints.registration);
+
+  Future<bool> _auth(String email, String password, String path) async {
+    try {
+      var response = await client.post(path, data: {
+        "email": email,
+        "password": password,
+      });
+      var tokens = JWTModel.fromJson(response.data);
+      await updateTokens(tokens);
+      if (response.statusCode == 200) return true;
+    } catch (e) {
+      print(e);
+    }
+    return false;
+  }
+
+  Future<void> updateTokens(JWTModel tokens) async {
+    _tokens = tokens;
+    await storageService.writeRefreshToken(tokens.refreshToken);
+  }
+
+  Future<void> tryAutoLogin() async {
+    String refreshToken = storageService.getRefreshToken();
+    var tokens = JWTModel(null, refreshToken);
+    await updateTokens(tokens);
+    if (tokens.refreshToken.isEmpty) {
+      isLoggedIn = false;
+      return;
+    } else {
+      bool refreshResult = await refresh();
+      isLoggedIn = refreshResult;
+    }
+  }
+
+  Future<AuthService> init() async {
+    return this;
+  }
+}
